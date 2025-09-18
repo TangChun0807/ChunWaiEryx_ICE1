64ec79c7d5abd9452a1317f5cc7a849e5b246c6b
TangChun0807 <tangchun@sheridancollege.ca>
2025-09-17 23:49:20 -0400

    Updated lucky card hard code

--- src/card/CardTrick.java
+++ src/card/CardTrick.java
@@ -21,6 +21,7 @@ public class CardTrick {
         Scanner input = new Scanner(System.in);
         boolean match = false;
         
+        
         for (int i=0; i<magicHand.length; i++)
         {
             Card c = new Card();
@@ -65,6 +66,24 @@ public class CardTrick {
             System.out.println("Your card was not in the magic hand");
         }
         // add one luckcard hard code 2,clubs
+        Card luckyCard = new Card();
+        luckyCard.setValue(2);
+        luckyCard.setSuit("Clubs");
+
+        boolean luckyMatch = false;
+            for(Card c : magicHand){
+            if(c.getValue() == luckyCard.getValue() && c.getSuit().equals(luckyCard.getSuit())){
+                match = true;
+                break;
+            }
+        }
+
+        if(luckyMatch){
+            System.out.print("Lucky card found! You win a bonus!");
     }
-    
+        else{
+
+    System.out.println("Lucky card not found. Better luck next time");
+}
+    }
 }
