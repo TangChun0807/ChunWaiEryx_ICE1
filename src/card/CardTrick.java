caa1379c9a13067b0ed51ffe5fc2ce8aa7fc5f54
TangChun0807 <tangchun@sheridancollege.ca>
2025-09-17 23:35:35 -0400

    added the logic and the output

--- src/card/CardTrick.java
+++ src/card/CardTrick.java
@@ -3,6 +3,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package card;
+import java.util.Scanner;
+import java.util.Random;
 
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
@@ -15,16 +17,51 @@ public class CardTrick {
     public static void main(String[] args)
     {
         Card[] magicHand = new Card[7];
+        Random rand = new Random();
+        Scanner input = new Scanner(System.in);
+      
         
         for (int i=0; i<magicHand.length; i++)
         {
             Card c = new Card();
+            c.setValue(rand.nextInt(13) + 1);
+            c.setSuit(Card.SUITS[rand.nextInt(4)]);
+            magicHand[i] = c;
+            System.out.println(c.getSuit() + " " +c.getValue() );
+            
+            
+            
             //c.setValue(insert call to random number generator here)
             //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
         }
         
         //insert code to ask the user for Card value and suit, create their card
         // and search magicHand here
+        System.out.print("Please pick a card 1 to 13: ");
+        int userInput = input.nextInt();
+        input.nextLine();
+        System.out.print("0 - 3 where 0 = Hearts  , 1 = Diamonds , 2 = Spades , 3 = Clubs: ");
+        int userSuit = input.nextInt();
+        
+        boolean match = false;
+        
+        for(Card c : magicHand){
+            if(c.getValue() == userInput && c.getSuit().equals(Card.SUITS[userSuit])){
+                match = true;
+                break;
+            }
+        }
+        if(match){
+            System.out.println("Your card was in the magic hand");
+        }
+        else{
+            System.out.println("Your card was not in the magic hand");
+        }
+        
+        
+        
+        
+        
         //Then report the result here
         // add one luckcard hard code 2,clubs
     }
